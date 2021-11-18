73267f0b5ff2874710d290998682528a68465813
Joako-Sador <joaquin.fernandez1@mail.udp.cl>
2021-11-18 19:23:11 -0300

    new commit

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,30 @@
+/* IMPORTS */
 const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
+
+
+//-------------------------------------------
+
+/* CONFIGS */
+
+const server = express()
+dotenv.config()
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+server.use(bodyParser.json())
+server.use(cors())
+
+/* VARIABLES */
+
+var port = process.env.PORT || 3000
+var ip = process.env.PORT || 'localhost'
+
+server.get('/', (req,res) => res.send('<h1>Server Operando</h1>'))
+
+/* PORTS */
+
+
 console.log('hola mundo')
