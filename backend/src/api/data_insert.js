636b11c7bafd08f31db999edc7c21358b8592cb6
Joako-Sador <joaquin.fernandez1@mail.udp.cl>
2021-11-19 19:53:27 -0300

    new commit

--- backend/src/api/data_insert.js
+++ backend/src/api/data_insert.js
@@ -2,10 +2,10 @@
 const { Router } = require('express')
 const express = require('express')
 const router = express.Router()
-const { usuarios } = require('../controllers/data_insert.controller')
+const { DataInserted } = require('../controllers/data_insert.controller')
 
 
 /* APIS */
-router.post('/insert',usuarios)
+router.post('/insert',DataInserted)
 
 module.exports = router
